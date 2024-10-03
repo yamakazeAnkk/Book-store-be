a752c14f19a80d4d2617018cd02c98602dec4db7
yamakazeAnkk <tuongankk2003@gmail.com>
2024-10-03 09:42:22 +0700

    not to save file json

--- BookStore/obj/Debug/net6.0/BookStore.AssemblyInfo.cs
+++ BookStore/obj/Debug/net6.0/BookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bfa6fe4caf612eedfd67bd6b0c4690b14ce844f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa82d42533a6b7edb8d6af0a308d4020b2ff31c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
